f63da609f32cb30e110be479187cf6b13f7df375
Aykhan Pashayev <aykhan.pashayev001@gmail.com>
2026-03-30 16:43:09 -0400

    /login added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,24 @@ import NavBar from "./components/NavBar";
 import IdentityRiskTable from "./pages/IdentityRiskTable";
 import IncidentFeed from "./pages/IncidentFeed";
 import IdentityDetail from "./pages/IdentityDetail";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <>
-      <NavBar />
       <Routes>
-        <Route path="/" element={<IdentityRiskTable />} />
-        <Route path="/incidents" element={<IncidentFeed />} />
-        <Route path="/identities/:arn" element={<IdentityDetail />} />
-        <Route path="*" element={<p className="empty-state">Page not found.</p>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={
+          <>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<IdentityRiskTable />} />
+              <Route path="/incidents" element={<IncidentFeed />} />
+              <Route path="/identities/:arn" element={<IdentityDetail />} />
+              <Route path="*" element={<p className="empty-state">Page not found.</p>} />
+            </Routes>
+          </>
+        } />
       </Routes>
     </>
   );
